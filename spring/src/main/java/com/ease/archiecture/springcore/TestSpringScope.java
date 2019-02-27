8af3131d1504dfffb4512095c9c48081c5f865cc
wei.jiang <wei.jiang@17zuoye.com>
2019-02-28 01:43:15 +0800

    add spring core

--- spring/src/main/java/com/ease/archiecture/springcore/TestSpringScope.java
+++ spring/src/main/java/com/ease/archiecture/springcore/TestSpringScope.java
@@ -3,10 +3,14 @@ package com.ease.archiecture.springcore;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class TestMain {
+public class TestSpringScope {
+
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("BeansTest.xml");
-        TestEntity testEntity = (TestEntity) applicationContext.getBean("testEntity");
+        TestEntity testEntity = (TestEntity) applicationContext.getBean("testEntity2");
+        testEntity.setMsg("Object A");
         System.out.println(testEntity.getMsg());
+        TestEntity testEntity1 = (TestEntity) applicationContext.getBean("testEntity2");
+        System.out.println(testEntity1.getMsg());
     }
 }
