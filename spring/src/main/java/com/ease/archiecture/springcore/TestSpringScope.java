cfa1c945d815e3d5c232dd0ea757db2971492b21
wei.jiang <wei.jiang@17zuoye.com>
2019-03-10 15:49:08 +0800

    add singleton test

--- spring/src/main/java/com/ease/archiecture/springcore/TestSpringScope.java
+++ spring/src/main/java/com/ease/archiecture/springcore/TestSpringScope.java
@@ -7,10 +7,10 @@ public class TestSpringScope {
 
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("BeansTest.xml");
-        TestEntity testEntity = (TestEntity) applicationContext.getBean("testEntity2");
+        TestEntity testEntity = (TestEntity) applicationContext.getBean("testEntity1");
         testEntity.setMsg("Object A");
         System.out.println(testEntity.getMsg());
-        TestEntity testEntity1 = (TestEntity) applicationContext.getBean("testEntity2");
+        TestEntity testEntity1 = (TestEntity) applicationContext.getBean("testEntity1");
         System.out.println(testEntity1.getMsg());
     }
 }
